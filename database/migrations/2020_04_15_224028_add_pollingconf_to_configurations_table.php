a06e2b4585d0fc697b8efa9bc00d179cf52604b6
fakhrip <fakhriputra123s@gmail.com>
2020-04-15 22:42:56 +0700

    add polling conf to migration file

---
+++ database/migrations/2020_04_15_224028_add_pollingconf_to_configurations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPollingconfToConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('configurations', function (Blueprint $table) {
+            $table->boolean('polling_activation')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('configurations', function (Blueprint $table) {
+            //
+        });
+    }
+}
