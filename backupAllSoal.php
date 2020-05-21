d1f5fee088084f51d65f5bbe6819bc32d84befb1
IritaSee <iga.narendra@gmail.com>
2020-05-22 00:17:54 +0800

    update method to echo all soal

--- backupAllSoal.php
+++ backupAllSoal.php
@@ -12,30 +12,56 @@ if ($conn->connect_error) {
 }
 for ($modul_id=1; $modul_id <25; $modul_id++)
 {
-    $sql = "SELECT moduls.judul FROM moduls WHERE moduls.id = ".$modul_id;
-    $result = $conn->query($sql);
-    echo "\n".$result."\n";
+    $sql = "SELECT moduls.judul,moduls.id FROM moduls WHERE moduls.id = ".$modul_id;
+    $result = $conn->query($sql)->fetch_assoc()['judul'];
+    echo "\n".$result."\n--------------------------\n";
+    
  //for ($dbsoal=1; $dbsoal<7; $dbsoal++){ 
-    //FITB
-    $sql = "SELECT soal__fitbs.soal FROM soal__fitbs WHERE soal__fitbs.modul_id = ".$modul_id;
-	$result = $conn->query($sql);
-    echo "FITB:"; 
-    echo "\n".$result."\n";
-    //jurnal
-    $sql = "SELECT soal__jurnals.soal FROM soal__jurnals WHERE soal__jurnals.modul_id = ".$modul_id;
-	$result = $conn->query($sql);
-    echo "Jurnal:"; 
-    echo "\n".$result."\n";	
-    //mandiri
-    $sql = "SELECT soal__mandiris.soal FROM soal__mandiris WHERE soal__mandiris.modul_id = ".$modul_id;
-	$result = $conn->query($sql);
-    echo "Mandiri:"; 
-    echo "\n".$result."\n";	
-    //TA
-    $sql = "SELECT soal__tas.pertanyaan, soal__tas.jawaban_benar FROM soal__tas WHERE soal__tas.modul_id = ".$modul_id;
-	$result = $conn->query($sql);
-    echo "Tes Awal (TA):"; 
-    echo "\n".$result."\n";	
+    echo "\n FITB \n";
+    $sql = "SELECT soal__fitbs.soal,soal__fitbs.id FROM soal__fitbs WHERE soal__fitbs.modul_id = ".$modul_id;
+    if ($hasil = $mysqli->query($sql))
+    {
+        while ($row = $hasil->fetch_assoc())
+        {
+            $soal=$row['soal'];
+            echo $soal."\n";
+        }
+        $hasil->free();
+    }
+    echo "\njurnal\n";
+    $sql = "SELECT soal__jurnals.soal,soal__jurnals.id FROM soal__jurnals WHERE soal__jurnals.modul_id = ".$modul_id;
+	if ($hasil = $mysqli->query($sql))
+    {
+        while ($row = $hasil->fetch_assoc())
+        {
+            $soal=$row['soal'];
+            echo $soal."\n";
+        }
+        $hasil->free();
+    }
+    echo "\n Mandiri \n";
+    $sql = "SELECT soal__mandiris.soal,soal__mandiris.id FROM soal__mandiris WHERE soal__mandiris.modul_id = ".$modul_id;
+    if ($hasil = $mysqli->query($sql))
+    {
+        while ($row = $hasil->fetch_assoc())
+        {
+            $soal=$row['soal'];
+            echo $soal."\n";
+        }
+        $hasil->free();
+    }
+    echo "\n Test awal: \n";
+    $sql = "SELECT soal__tas.pertanyaan, soal__tas.jawaban_benar,soal__tas.id FROM soal__tas WHERE soal__tas.modul_id = ".$modul_id;
+    if ($hasil = $mysqli->query($sql))
+    {
+        while ($row = $hasil->fetch_assoc())
+        {
+            $soal=$row['pertanyaan'];
+            $jawaban=$row['jawaban_benar'];
+            echo "Pertanyaan: ".$soal."\n Jawaban Benar: ".$jawaban;
+        }
+        $hasil->free();
+    }	
 
 	
 	/*if ($result->num_rows > 0) {
